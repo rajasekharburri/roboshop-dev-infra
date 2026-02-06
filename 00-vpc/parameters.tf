7812c88b0d5f7f0a7b9a2b7f889aa3458852b9fb
rajasekhar burri <burriraja@gmail.com>
2026-02-06 14:13:05 +0530

    terraform updates

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -1,8 +1,8 @@
-# resource "aws_ssm_parameter" "vpc_id" {
-#   name  = "/${var.project_name}/${var.environment}/vpc_id"
-#   type  = "String"
-#   value = module.vpc.vpc_id
-# }
+resource "aws_ssm_parameter" "vpc_id" {
+  name  = "/${var.project_name}/${var.environment}/vpc_id"
+  type  = "String"
+  value = module.vpc.vpc_id
+}
 
 # resource "aws_ssm_parameter" "public_subnet_ids" {
 #   name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
@@ -20,4 +20,4 @@
 #   name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
 #   type  = "StringList"
 #   value = join("," , module.vpc.database_subnet_ids)
-# }
+# } 
--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,18 +1,18 @@
- # Using Open source module
-module "catalogue" {
-  source = "terraform-aws-modules/security-group/aws"
 
-  name        = "${local.common_name_suffix}-catalogue"
-  use_name_prefix = false
-  description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
-  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+# module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
 
-} 
+#   name        = "${local.common_name_suffix}-catalogue"
+#   use_name_prefix = false
+#   description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
+#   vpc_id      = data.aws_ssm_parameter.vpc_id.value
+
+# } 
   
-
+#  # Using Open source module
 module "sg" {
   count = length(var.sg_names)
-  source = "git::https://github.com/daws-86s/terraform-aws-sg.git?ref=main"
+  source ="git::https://github.com/rajasekharburri/terraform-aws-sg.git"
   project_name = var.project_name
   environment = var.environment
   sg_name = var.sg_names[count.index]
